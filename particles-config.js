380abf4f7adb833beb3dcf708e2ac7734cfd71d8
AryanPROFFESOR <145572344+AryanPROFFESOR@users.noreply.github.com>
2025-06-30 09:43:19 +0530

    Create particles-config.js

---
+++ particles-config.js
@@ -0,0 +1,44 @@
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 60
+    },
+    "color": {
+      "value": "#000000"
+    },
+    "shape": {
+      "type": "circle"
+    },
+    "opacity": {
+      "value": 0.5
+    },
+    "size": {
+      "value": 3
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#000000",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 2
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      }
+    }
+  },
+  "retina_detect": true
+});
