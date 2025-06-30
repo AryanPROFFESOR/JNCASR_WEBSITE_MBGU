ee96fa5d899e4c06d6b436b68f4b230666ed8c6e
AryanPROFFESOR <145572344+AryanPROFFESOR@users.noreply.github.com>
2025-06-30 17:32:27 +0530

    Update particles-config.js

--- particles-config.js
+++ particles-config.js
@@ -1,30 +1,46 @@
 particlesJS("particles-js", {
   "particles": {
     "number": {
-      "value": 60
-    },
-    "color": {
-      "value": "#000000"
+      "value": 25,  // You can adjust this number (more or fewer DNAs)
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
     },
     "shape": {
-      "type": "circle"
+      "type": "image",
+      "image": {
+        "src": "DNA_IMAGE.png", // your file, should be in same folder
+        "width": 100,
+        "height": 100
+      }
     },
     "opacity": {
-      "value": 0.5
+      "value": 0.8,
+      "random": false
     },
     "size": {
-      "value": 3
+      "value": 20,
+      "random": true
     },
     "line_linked": {
       "enable": true,
       "distance": 150,
       "color": "#000000",
-      "opacity": 0.4,
+      "opacity": 0.5,
       "width": 1
     },
     "move": {
       "enable": true,
-      "speed": 2
+      "speed": 2,
+      "direction": "none",
+      "random": true,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": true,
+      "attract": {
+        "enable": false
+      }
     }
   },
   "interactivity": {
@@ -32,11 +48,23 @@ particlesJS("particles-js", {
     "events": {
       "onhover": {
         "enable": true,
-        "mode": "repulse"
+        "mode": "grab" // DNA images connect when cursor near
       },
       "onclick": {
         "enable": true,
         "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 200,
+        "line_linked": {
+          "opacity": 0.7
+        }
+      },
+      "push": {
+        "particles_nb": 4
       }
     }
   },
